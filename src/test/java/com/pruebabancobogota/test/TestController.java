0f7a5f229b2bcd9774b3304b41ce90dad1a4b788
cazb999 <cazb999@hotmail.com>
2020-06-22 18:30:33 -0500

    TEsts

--- src/test/java/com/pruebabancobogota/test/TestController.java
+++ src/test/java/com/pruebabancobogota/test/TestController.java
@@ -3,11 +3,21 @@ package com.pruebabancobogota.test;
 import com.pruebabancobogota.prueba.controller.EmployeeController;
 import com.pruebabancobogota.prueba.model.Employee;
 import com.pruebabancobogota.prueba.service.IEmployeeService;
+import com.pruebabancobogota.prueba.vo.EmployeeVo;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
@@ -23,11 +33,13 @@ public class TestController {
     @Mock
     private IEmployeeService service;
 
-
     @Test
-    public void givenDTOIsValid_whenCallController_thenReturnEmployees() {
-        List<Employee> listEmployees = new ArrayList<>();
-        listEmployees.add(new Employee(1,"carlos","developer"));
-        lenient().when(controller.getEmployees()).thenReturn(listEmployees);
+    public void given_whenGetEmployees_thenReturnEMployees() {
+        List<Employee> employess = new ArrayList<>();
+        employess.add(new Employee(1, "carlos zambrano", "Developer"));
+        when(service.getEmployees()).thenReturn(employess);
+        List<Employee> list = controller.getEmployees();
+        Assert.assertEquals(list.size(), employess.size());
+
     }
 }
